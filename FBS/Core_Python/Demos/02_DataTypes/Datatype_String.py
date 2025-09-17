ac72a4f9708e4b7f3408bb03e6a776ccef0565ab
Sukdeo L Kapade <sukdeol@kpit.com>
2025-09-17 20:18:43 +0530

    basic string slicing

---
+++ FBS/Core_Python/Demos/02_DataTypes/Datatype_String.py
@@ -0,0 +1,17 @@
+#String Slicing
+text = 'Hello, Python!'
+print(text)
+
+#substring = original_string[start:end:step]
+#print(text[0:5]) #Hello
+#print(text[5:11])  #Python
+#print(text[7:13:3]) #Ph
+#print(text[5::2]) #,Pto!
+#print(text[5::-1])  #,olleH
+#print(text[-5::-1])  #tyP ,olleH
+print(text[-5:-13:-1])   #tyP ,oll
+
+'''
+Index:  -14 -13 -12 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+Text:    H   e   l   l   o  ,     P  y  t  h  o  n  !
+'''
