e7c057f21087af612a2fd871094706e9e1ba744c
Sukdeo Kapade <kapadesukdeo@gmail.com>
2025-09-26 19:05:37 +0530

    updated folder

--- FBS/Core_Python/Demos/02_DataTypes/Datatype_String.py
+++ FBS/Core_Python/Demos/02_DataTypes/Datatype_String.py
@@ -1,15 +1,17 @@
 #String Slicing
-text = 'Hello, Python!'
+text = 'Hello,_Python!'
 print(text)
 
 #substring = original_string[start:end:step]
-#print(text[0:5]) #Hello
+#print(text[0:7]) #Hello
 #print(text[5:11])  #Python
 #print(text[7:13:3]) #Ph
 #print(text[5::2]) #,Pto!
 #print(text[5::-1])  #,olleH
 #print(text[-5::-1])  #tyP ,olleH
-print(text[-5:-13:-1])   #tyP ,oll
+#print(text[-5:-13:-1])   #tyP ,oll
+#print(text[0])
+print(text[3:6])
 
 '''
 Index:  -14 -13 -12 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
