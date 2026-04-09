b4d4cda973500549f4b0da5ace4182d31b0a29c7
sukdeo12 <kapadesukdeo@gmail.com>
2026-04-09 21:58:27 +0530

    revision

---
+++ FBS/Core_Python/x_Revision/2 _basic_datatypes.py
@@ -0,0 +1,77 @@
+# var = 10
+# print(var)
+# print(type(var))
+# x* = 10.8
+# print(f'{x*} type is = {type(x*)} id is = {hex(id(x*))}')
+
+# #complex number
+# x = 10.9
+# y = 10.9
+# print(f'{x} type is = {type(x)} id is = {hex(id(x))}')
+# print(f'{y} type is = {type(y)} id is = {hex(id(y))}')
+
+#Triple single code for string
+# str =   '''
+#         Hello word
+#         Everything is Good 
+#         Looks like
+#         '''
+# print(f'{str} type is = {type(str)} id is = {hex(id(str))}')
+# strw =   '''
+#         Hello word
+#         Everything is Good 
+#         Looks like
+#         '''
+# print(f'{strw} type is = {type(strw)} id is = {hex(id(strw))}')
+
+#Sequential Data type
+#1 - List  -> Mutable :: CHangeble
+# li = [4]
+# print(f'{li} type is = {type(li)} id is = {hex(id(li))}')
+# li2 = [4]
+# print(f'{li2} type is = {type(li2)} id is = {hex(id(li2))}')
+#id must be changed fro li and li2 if the dATA IS SAME 
+# [4] type is = <class 'list'> id is = 0x240d87f60c0
+# [4] type is = <class 'list'> id is = 0x240d8a20500
+
+#Tuple -> Inmutable :: Non Changable
+# li = ()
+# print(f'{li} type is = {type(li)} id is = {hex(id(li))}')
+# li2 = ()
+# print(f'{li2} type is = {type(li2)} id is = {hex(id(li2))}')
+#Id must same for li and li2 if data is same
+# () type is = <class 'tuple'> id is = 0x7ffa169375c0
+# () type is = <class 'tuple'> id is = 0x7ffa169375c0
+
+#set -> Mutable :: Changable
+# li = {}
+# print(f'{li} type is = {type(li)} id is = {hex(id(li))}')
+# li2 = {}
+# print(f'{li2} type is = {type(li2)} id is = {hex(id(li2))}')/
+
+# fruits = {"apple", "banana", "cherry", "apple"}  
+# print(fruits)
+# fruits.add("orange")
+# print(fruits) 
+# fruits.remove('banana')
+# print(fruits)
+
+# # Set operations
+# set_a = {1, 2, 3, 4}
+# set_b = {3, 4, 5, 6}
+
+# print(set_a.union(set_b))        # {1, 2, 3, 4, 5, 6}
+# print(set_a.intersection(set_b)) # {3, 4}
+# print(set_a.difference(set_b))   # {1, 2}
+
+# numbers = [1, 2, 2, 3, 4, 4, 5]
+# print(numbers)
+# print(type(numbers))
+# unique_numbers = set(numbers)
+# print(unique_numbers)  # {1, 2, 3, 4, 5}
+# print(type(unique_numbers))
+# numbers = list(unique_numbers)
+# print(numbers)
+# print(type(numbers))
+
+
---
+++ FBS/Core_Python/x_Revision/3_Operators.py
@@ -0,0 +1,14 @@
+# num1 = 10 #Dynamic numbers
+# num2 = 20
+# sum = num1 + num2
+# print('Addition of ', num1, 'and', num2, ' is = ', sum)
+# print('Addition of '+ str(num1) + ' and ' + str(num2) + ' is = ' + str(sum))
+# print(f'addition of {num1} and {num2} is {sum}')
+# string = f'addition of {num1} and {num2} is {sum}'
+# print(string)
+
+#How to create user interation here so will use input()
+num1 = int(input('ENter the numbers 1 - '))
+num2 = int(input('ENter the numbers 2 - '))
+sum = num1 + num2
+print(f'addition of {num1} and {num2} is {sum}')
